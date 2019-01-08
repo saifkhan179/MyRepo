269157fefd45647b2f3bf3230cb35a842a2175f3
saifkhan179 <mkhan@sysgain.com>
2019-01-07 16:05:23 -0800

    Add files via upload

---
+++ bucket.tf
@@ -0,0 +1,38 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {
+    default = "us-phoenix-1"
+}
+variable "namespace_name" {
+    default = ""
+}
+
+variable "compartment_ocid" {}
+
+variable "deployment_short_id" {}
+
+
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region = "${var.region}"
+}
+
+resource "oci_objectstorage_bucket" "t" {
+  compartment_id = "${var.compartment_ocid}"
+  name = "BucketOne${var.deployment_short_id}"
+  access_type = "ObjectRead" // or NoPublicAccess
+  namespace = "${var.namespace_name}"
+  metadata = {
+    "foo" = "bar"
+  }
+}
+
+output "Bucket_name" {
+  value = ["${oci_objectstorage_bucket.t.name}"]
+}
+
